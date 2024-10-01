02acd6c440b048473ec92b34ccb9452838953433
Seth Way <way.sethaniel@gmail.com>
2024-10-01 12:50:03 -0400

    add footer with links to socials

--- src/components/ui/NavIcon/NavIcon.jsx
+++ src/components/ui/NavIcon/NavIcon.jsx
@@ -1,9 +1,29 @@
 import './NavIcon.css';
 
-const NavIcon = ({isOpen, handleClick}) => {
+const NavIcon = ({ isOpen, handleClick }) => {
 	return (
-		<svg id="nav-icon" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" onClick={handleClick}>
-			<circle cx="50" cy="50" r="45" />
+		<svg
+			id="nav-icon"
+			className={isOpen ? 'open' : ''}
+			viewBox="0 0 100 100"
+			xmlns="http://www.w3.org/2000/svg"
+			onClick={handleClick}
+			stroke="var(--text)"
+			strokeWidth={8}
+			strokeLinecap="round"
+			fill="var(--background">
+			<g id="g1">
+				<path d="M10,20 L90,20" />
+				<circle cx="70" cy="20" r="10" />
+			</g>
+			<g id="g2">
+				<path d="M10,50 L90,50" />
+				<circle cx="30" cy="50" r="10" />
+			</g>
+			<g id="g3">
+				<path d="M10,80 L90,80" />
+				<circle cx="60" cy="80" r="10" />
+			</g>
 		</svg>
 	);
 };
